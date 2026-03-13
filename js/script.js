f46ce840305a5b5554b8a70f251b75fcbccbaa0c
jrw <197988939+Jrwick@users.noreply.github.com>
2026-03-13 22:38:27 +0000

    Add operand variables, operator variable, and  operate function

--- js/script.js
+++ js/script.js
@@ -17,3 +17,22 @@ function divide(num1, num2) {
     return num1 / num2;
   }
 }
+
+let leftOperand;
+let rightOperand;
+let operator;
+
+function operate(leftOperand, rightOperand, operator) {
+  switch (operator) {
+    case "add":
+      return add(leftOperand, rightOperand);
+    case "subtract":
+      return subtract(leftOperand, rightOperand);
+    case "multiply":
+      return multiply(leftOperand, rightOperand);
+    case "divide":
+      return divide(leftOperand, rightOperand);
+    default:
+      return "Invalid operator";
+  }
+}
