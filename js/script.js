e9c8ec154119a714b35668b8ad5918864e211192
jrw <197988939+Jrwick@users.noreply.github.com>
2026-03-13 22:26:55 +0000

    Add functions for add, subtract, multiply, and divide

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-alert("testing");
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  if (num2 == 0) {
+    return "Error";
+  } else {
+    return num1 / num2;
+  }
+}
